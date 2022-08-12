2a6fec490355eb4d7569fa380d9bc2b34c9b2e5e
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-08-12 15:29:02 +0200

    improved the UI by replacing static input placeholder with a moving label

--- js/modals/addEditModalStructure.js
+++ js/modals/addEditModalStructure.js
@@ -1,44 +1,47 @@
 export const addEditModalStructure = `<form>
                         <div class="info-one">
                             <div class="name-input">
-                                <input type="text" id="name" name="name" placeholder="Name*"
-                                    onfocus="this.placeholder=''" onblur="this.placeholder='Name*'" required>
+                                <input type="text" id="name" name="name" required>
+                                    <label>Name<span>*</span></label>
                                     <div class="error-hint">invalid name</div>
                                     <div class="error-hint-required">name is required</div>
                             </div>
+
                             <div class="surname-input">
-                                <input type="text" id="surname" name="surname" placeholder="Surname*"
-                                    onfocus="this.placeholder=''" onblur="this.placeholder='Surname*'" required>
+                                <input type="text" id="surname" name="surname" required>
+                                    <label>Surname<span>*</span></label>
                                     <div class="error-hint">invalid surname</div>
                                     <div class="error-hint-required">surname is required</div>
+                            </div>
+                            </div>
 
-                            </div>
-                            </div>
                             <div class="info-two">
                             <div class="phone-input">
-                                <input type="text" id="phone" name="phone" maxlength="9" placeholder="Phone*" onfocus="this.placeholder=''" onblur="this.placeholder='Phone*'" required>
+                                <input type="text" id="phone" name="phone" maxlength="9" required>
+                                    <label>Phone<span>*</span></label>
                                     <div class="error-hint">invalid number</div>
                                     <div class="error-hint-required">number is required</div>
                                     <div class="error-hint-number">number already exists</div>
-
                             </div>
+
                             <div class="mail-input">
-                                <input type="email" id="email" name="email" placeholder="Email"
-                                    onfocus="this.placeholder=''" onblur="this.placeholder='Email'" required>
+                                <input type="text" id="email" name="email" required>
+                                    <label>Email</label>
                                     <div class="error-hint">invalid email</div>
                             </div>
                             </div>
                         </form>
                     </div>
+
                     <div class="new-con-secondary-info">
                         <form>
                             <div class="address-input">
-                                <input type="text" id="address" name="address" placeholder="Address"
-                                    onfocus="this.placeholder=''" onblur="this.placeholder='Address'">
+                                <input type="text" id="address" name="address" required>
+                                    <label>Address</label>
                             </div>
                             <div class="notes-input">
-                                <input type="text" id="notes" name="notes" maxlength="36" placeholder="Notes"
-                                    onfocus="this.placeholder=''" onblur="this.placeholder='Notes'">
+                                <input type="text" id="notes" name="notes" maxlength="36" required>
+                                    <label>Notes</label>
                             </div>
                         </form>
                     </div>
