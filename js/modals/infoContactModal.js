e604e30b34737bd510bf0c3297a2f9ed5de866c5
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-08-15 00:04:10 +0200

    changed a structure and styles of contacts' info container

--- js/modals/infoContactModal.js
+++ js/modals/infoContactModal.js
@@ -8,41 +8,68 @@ export const infoContactModal = () => {
     if (e.target.closest(".contact p:first-child")) {
       mainModule.modalOverlay.classList.add("open-modal");
       mainModule.modalContainerInfo.classList.add("open-modal");
-      const conNumber = e.target.parentElement.parentElement.id;
+      const conNumber = e.target.closest("li").id;
 
       mainModule.peopleData.find((i) => {
-        const { name, surname, phone, mail, address, notes } = i;
+        const {
+          name,
+          surname,
+          phone,
+          mail,
+          address,
+          notes,
+          img: { src },
+        } = i;
         if (phone === conNumber) {
           mainModule.modalContainerInfo.innerHTML = `
-                        <div class="btn-container"><i class="fas fa-times"></i></div>
-                        <div class="top-info">
-                        <div class="photo no-select">
-                        <div class="img">
-                        ${name.slice(0, 1)}${surname.slice(0, 1)}</div>
+                     <div class="top_info">
+                      <i class="fas fa-times"></i>
+                      <div class="top_info-avatar">
+                      ${
+                        src
+                          ? "<img src=" + src + " alt='contact img' />"
+                          : "<p>" +
+                            name.slice(0, 1) +
+                            surname.slice(0, 1) +
+                            "</p>"
+                      }
+                      </div>
                         </div>
-                         <div class="main-info">
-                         <p>${name}</p>
-                         <p>${surname}</p>
-                         <p><i class="fas fa-phone-alt no-select"></i>
-                         ${phone.replace(
-                           /(?!^)(?=(?:\d{3})+(?:\.|$))/gm,
-                           " "
-                         )}</p>
+                         <div class="bottom_info">
+                         <div class="bottom_info-name">
+                              <p>${name} ${surname}</p>
+                              <div class="bottom_info-name-underline"></div>
+                         
                          </div>
-                        </div>
-                         <div class="bottom-info">
-                <p><i class="fas fa-at no-select"></i>
-                 ${mail ? mail : "<span>no email address passed</span>"}</p>
-                <p><i class="fas fa-map-marker-alt no-select">&nbsp;</i>
-                 ${address ? address : "<span>no address passed</span>"}</p>
-                <p><i class="far fa-edit no-select"></i>
-                 ${notes ? notes : "<span>no notes passed</span>"}</p>
+                         <div class="bottom_info-details"> 
+                         <p>
+                         <i class="fas fa-phone-alt no-select"></i>
+                         ${phone.replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, " ")}
+                         </p>
+                          <p>
+                          <i class="fas fa-at no-select"></i>
+                          ${
+                            mail ? mail : "<span>no email address passed</span>"
+                          }
+                          </p>
+                          <p>
+                          <i class="fas fa-map-marker-alt no-select">&nbsp;</i>
+                          ${
+                            address ? address : "<span>no address passed</span>"
+                          }
+                          </p>
+                          <p>
+                          <i class="far fa-edit no-select"></i>
+                          ${notes ? notes : "<span>no notes passed</span>"}
+                          </p>
+                         </div>
+     
                 </div>`;
         }
       });
 
       mainModule.modalContainerInfo
-        .querySelector(".btn-container .fas")
+        .querySelector(".top_info .fas")
         .addEventListener("click", () => {
           mainModule.modalOverlay.classList.remove("open-modal");
           mainModule.modalContainerInfo.classList.remove("open-modal");
